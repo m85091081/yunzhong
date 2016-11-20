0518bec004b26a9f61e58e098fb02966fbabdd50
Sean2525 <madness48596@gmail.com>
2016-11-20 14:48:56 +0800

    add visitcount

--- core/index.py
+++ core/index.py
@@ -2,7 +2,7 @@
 # muMDAU_app main / first page 
 from core import app, socketio
 from flask import request, render_template, Blueprint, url_for, redirect, session
-from core_module.dbmongo import User
+from core_module.dbmongo import User,Visit
 from core_module.form import loginForm
 main = Blueprint('main', __name__ , template_folder='../core_template/templates')
 
@@ -11,6 +11,7 @@ def index():
     fbreg = request.cookies.get('fbreg') 
     loginform = loginForm()
     allmem = User.count('all')
+    Visit.incount()
     company = User.count('company')
     return render_template('index.html',**locals())
 
